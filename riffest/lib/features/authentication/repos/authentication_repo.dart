6ad788917ed835abf3a658373f40bc8902dd219e
ynjch97 <ynjch2017@naver.com>
2024-07-03 17:57:48 +0900

    Firebase > 이메일로 회원가입, 로그인, 로그아웃

--- riffest/lib/features/authentication/repos/authentication_repo.dart
+++ riffest/lib/features/authentication/repos/authentication_repo.dart
@@ -1,1 +1,37 @@
-class AuthenticationRepository {}
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AuthenticationRepository {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  User? get user => _firebaseAuth.currentUser; // 로그인한 사용자는 Nullable
+  bool get isLoggedIn => user != null;
+
+  Stream<User?> authStateChanges() => _firebaseAuth.authStateChanges();
+
+  Future<UserCredential> emailSignUp(String email, String password) async {
+    return await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> emailSignIn(String email, String password) async {
+    await _firebaseAuth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> emailSignOut() async {
+    await _firebaseAuth.signOut();
+  }
+}
+
+final authRepo = Provider((ref) => AuthenticationRepository());
+
+// StreamProvider : 변화를 바로 감지할 수 있음 => 유저 인증 상태 변경을 감지 (로그인/로그아웃)
+final authStateStream = StreamProvider((ref) {
+  final repo = ref.read(authRepo);
+  return repo.authStateChanges();
+});
